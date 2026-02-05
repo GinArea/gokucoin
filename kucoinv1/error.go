05c3146f23e38b9e4e772a3d2227ad52cd68f98c
Oleg Kuzmenko <>
2026-02-05 09:07:28 +0300

    error codes

--- kucoinv1/error.go
+++ kucoinv1/error.go
@@ -33,8 +33,6 @@ func (o *Error) ApiKeyInvalid() bool {
 		"400003", // KC-API-KEY not exists
 		"400004", // KC-API-PASSPHRASE error
 		"400005", // Signature Error
-		"400006", // The requested ip address is not in the api whitelist
-		"400007", // Access Denied
 	}
 	return slices.Contains(codes, o.Code)
 }
@@ -47,13 +45,16 @@ func (o *Error) TooManyVisits() bool {
 }
 
 func (o *Error) UnmatchedIp() bool {
-	return o.Code == "400006"
+	return o.Code == "400006" // 	The requested ip address is not on the api whitelist
 }
 
 func (o *Error) InsufficientBalance() bool {
 	codes := []string{
-		"300000", // Balance insufficient
-		"300003", // Insufficient balance
+		"102421", // spot: Insufficient account balance
+		"200004", // spot: Balance insufficient!
+		"200005", // Insufficient balance (insufficient balance when modifying risk limit)
+		"300003", // Balance not enough, please first deposit at least 2 USDT before you go to battle
+		"400100", // account.available.amount -- Insufficient balance
 	}
 	return slices.Contains(codes, o.Code)
 }
@@ -62,49 +63,31 @@ func (o *Error) Timeout() bool {
 	codes := []string{
 		"100002", // System Error
 		"500000", // Internal Server Error
+		"1015",   // cloudflare frequency limit according to IP, block 30s
 	}
 	return slices.Contains(codes, o.Code)
-}
-
-func (o *Error) OrderNotExist() bool {
-	return o.Code == "400100" // Order not exists
 }
 
 func (o *Error) SymbolNotExists() bool {
-	return o.Code == "100004" // Parameter error (used for invalid symbol)
-}
-
-func (o *Error) TradingDisabled() bool {
 	codes := []string{
-		"200002", // Trading is off
-		"200003", // Limit order is disabled
-		"200004", // Market order is disabled
+		"600202", // Request failed because the symbol ... was not available
+		"600203", // Symbol XXX-XXX cant be traded -- The symbol is not enabled for trading, such as downtime for upgrades, etc.
+		"900001", // spot: symbol does not exist
 	}
 	return slices.Contains(codes, o.Code)
-}
 
-func (o *Error) OrderFrozen() bool {
-	return o.Code == "411100" // Order frozen
 }
 
 func (o *Error) ReduceOnlyError() bool {
 	codes := []string{
-		"300014", // Reduce only order can not increase position
+		"300014", // The position is being liquidated, unable to place/cancel the order. Please try again later.
 		"300009", // No open positions to close
 	}
 	return slices.Contains(codes, o.Code)
 }
 
-func (o *Error) InvalidOrderPrice() bool {
-	return o.Code == "300011" // Unsupported order price
-}
-
 func (o *Error) InvalidOrderQuantity() bool {
 	codes := []string{
-		"300001", // Order quantity less than the minimum requirement
-		"300002", // Order quantity greater than the maximum limit
-		"300010", // Unsupported order quantity
-
 		"100001", // Futures invalid Size: "Please specify one of the following order units: qty (for underlying currency), size (for contracts), or valueQty (for value)."
 		"400760", // spot SELL: The order funds should more than ... USDT
 		"600101", // spot BUY: The order funds should more than ... USDT
@@ -112,20 +95,8 @@ func (o *Error) InvalidOrderQuantity() bool {
 	return slices.Contains(codes, o.Code)
 }
 
-func (o *Error) InvalidLeverage() bool {
-	return o.Code == "300012" // Unsupported leverage
-}
-
-func (o *Error) SignatureError() bool {
-	return o.Code == "100005" // Signature error
-}
-
 func (o *Error) UnknownCurrency() bool {
 	return o.Code == "111001" // Currency does not exist
-}
-
-func (o *Error) OrderIdIsDuplicate() bool {
-	return o.Code == "102426" // Duplicate user-defined unique order ID
 }
 
 func (o *Error) ClientOIdIsDuplicate() bool {
@@ -136,7 +107,17 @@ func (o *Error) ClientOIdIsDuplicate() bool {
 
 func (o *Error) IncorrectTradingMode() bool {
 	codes := []string{
+		"330012", // Your order is in One-Way Mode, while your account is set to Hedge Mode. Update your settings so they match and try again
 		"330013", // Your order is in Hedge Mode, while your account is set to One-Way Mode. Update your settings so they match and try again
+	}
+	return slices.Contains(codes, o.Code)
+}
+
+func (o *Error) AccessDenied() bool {
+	codes := []string{
+		"400007", // Access Denied
+		"40010",  // Unavailable to place orders. Your identity information/IP/phone number shows you're in a country/region that is restricted from this service.
+		"411100", // User is frozen -- Please contact us via support center
 	}
 	return slices.Contains(codes, o.Code)
 }
